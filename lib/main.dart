e3ab873d2da799a61a32b3e8ac3c5903be58edc2
Muhamad <nurul.ishlah@gmail.com>
2021-12-12 21:27:03 +0700

    Move photos model into separated file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
-import 'dart:convert';
-
-import 'package:flutter/foundation.dart';
+import 'package:api_demo/photo_model.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -53,44 +51,6 @@ class PhotosList extends StatelessWidget {
       itemBuilder: (context, index) {
         return Image.network(photos[index].thumbnailUrl);
       },
-    );
-  }
-}
-
-List<Photo> parsePhotos(String responseBody) {
-  final parsed = jsonDecode(responseBody).cast<Map<String, dynamic>>();
-  return parsed.map<Photo>((json) => Photo.parseJson(json)).toList();
-}
-
-Future<List<Photo>> fetchPhotos(http.Client client) async {
-  final response = await client
-      .get(Uri.parse('https://jsonplaceholder.typicode.com/photos'));
-
-  return compute(parsePhotos, response.body);
-}
-
-class Photo {
-  final int albumId;
-  final int id;
-  final String title;
-  final String url;
-  final String thumbnailUrl;
-
-  const Photo({
-    required this.albumId,
-    required this.id,
-    required this.title,
-    required this.url,
-    required this.thumbnailUrl,
-  });
-
-  factory Photo.parseJson(Map<String, dynamic> data) {
-    return Photo(
-      albumId: data['albumId'] as int,
-      id: data['id'] as int,
-      title: data['title'] as String,
-      url: data['url'] as String,
-      thumbnailUrl: data['thumbnailUrl'] as String,
     );
   }
 }
